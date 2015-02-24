2e6f2fc7b05c71ee49e610568b0966a0f68e8446
sterlingbaldwin <baldwin32@llnl.gov>
2015-02-24 15:22:09 -0800

    grids now correctly count the number of cells taken in every direction

--- acme_site/static/js/grid.js
+++ acme_site/static/js/grid.js
@@ -2,6 +2,8 @@ $('body').ready(function(){
     
   	var gridster;
     var maxCols = 6;
+    var maxHeight = 6;
+    var resize_handle_html = '<span class="gs-resize-handle gs-resize-handle-both"></span>'
 
 	gridster = $(".gridster ul").gridster({
 	    widget_margins: [10, 10],
@@ -50,15 +52,21 @@ $('body').ready(function(){
     });
   
 
+  /**
+   * Registers call backs for window creation buttons
+   */
 	function add_grid(name){
     if($('#' + name + '_window').length == 0) {
-      var widget = ['<li id=' + name + '_window>' + name + '</li>',1,1];
+      var widget = ['<li id=' + name + '_window></li>',1,1];
   		gridster.add_widget.apply(gridster,widget);
-      $("#" + name).bind
+      $('#' + name).bind
     }
     sizeFixup();
 	}
 
+  /**
+  * Computes and sets the size for each window
+  */
   function sizeFixup() {
     var i = 0;
     var windows = $(".gs-w");
@@ -66,34 +74,20 @@ $('body').ready(function(){
       var nodesInCol = 1;
       var nodesInRow = 1;
       var j = 0;
-      for(; j < windows.length; j++) { 
-        if (i != j) {
-          if( parseInt($(windows[i]).attr('data-row')) == parseInt($(windows[j]).attr('data-row')) 
-              || ( parseInt($(windows[i]).attr('data-row'))-1 <= parseInt($(windows[j]).attr('data-row'))-2+parseInt($(windows[j]).attr('data-sizey')) 
-                  && parseInt($(windows[i]).attr('data-row')) >= parseInt($(windows[j]).attr('data-row')) ) ) 
-          {
-            //console.log($(windows[i]).attr('id') + " is in the same row as " + $(windows[j]).attr('id'));
-            nodesInRow++;
-          }
-          if(parseInt($(windows[i]).attr('data-col')) == parseInt($(windows[j]).attr('data-col'))
-              || (parseInt($(windows[i]).attr('data-col'))-1 <= parseInt($(windows[j]).attr('data-col'))-2+parseInt($(windows[j]).attr('data-sizex'))
-                  && parseInt($(windows[i]).attr('data-col')) >= parseInt($(windows[j]).attr('data-col')) ) ) 
-          {
-            //console.log($(windows[i]).attr('id') + " is in the same col as " + $(windows[j]).attr('id'));
-            nodesInCol++;
-          }
-        }
-      }
-      console.log($(windows[i]).attr('id') + ' has ' + nodesInRow + ' other nodes in its row');
-      console.log($(windows[i]).attr('id') + ' has ' + nodesInCol + ' other nodes in its col');
+      get_windows({
+        id: $(windows[i]).attr('id'),
+        x: parseInt($(windows[i]).attr('data-col')),
+        y: parseInt($(windows[i]).attr('data-row')),
+      });
      // resize_widget($(windows[i]), )
       if ( ($(windows[i]).attr('data-col')-1) % (maxCols/nodesInRow) != 0 && nodesInRow < 4) {
-        console.log('moving ' + $(windows[i]).attr('id') + ' to ' + (parseInt($(windows[i]).attr('data-col'))+(Math.floor(maxCols/nodesInRow) - (parseInt($(windows[i]).attr('data-col'))-1)%Math.floor(maxCols/nodesInRow))));
+        //console.log('moving ' + $(windows[i]).attr('id') + ' to ' + (parseInt($(windows[i]).attr('data-col'))+(Math.floor(maxCols/nodesInRow) - (parseInt($(windows[i]).attr('data-col'))-1)%Math.floor(maxCols/nodesInRow))));
         if (nodesInRow == 1) {
           var newCol = 1;
         } else {
           newCol = parseInt($(windows[i]).attr('data-col'))+(Math.floor(maxCols/nodesInRow) - (parseInt($(windows[i]).attr('data-col'))-1)%Math.floor(maxCols/nodesInRow));
         }
+        /*
         gridster.mutate_widget_in_gridmap($(windows[i]), {
           size_x: parseInt($(windows[i]).attr('data-sizex')),
           size_y: parseInt($(windows[i]).attr('data-sizey')),
@@ -105,23 +99,73 @@ $('body').ready(function(){
           col: newCol,
           row: parseInt($(windows[i]).attr('data-row'))
         });
+*/
       }
       get_rows_cols({
-      id: $(windows[i]).attr('id'),
-      x: parseInt($(windows[i]).attr('data-col')),
-      y: parseInt($(windows[i]).attr('data-row')),
-      sizex: parseInt($(windows[i]).attr('data-sizex')),
-      sizey: parseInt($(windows[i]).attr('data-sizey'))
-    });
+        id: $(windows[i]).attr('id'),
+        x: parseInt($(windows[i]).attr('data-col')),
+        y: parseInt($(windows[i]).attr('data-row')),
+        sizex: parseInt($(windows[i]).attr('data-sizex')),
+        sizey: parseInt($(windows[i]).attr('data-sizey'))
+      });
     }
   }
 
-  /*
-    widget -> x, y, sizex, sizey
 
-    returns the number of widgets to the left, right, above, and below the requested widget
-    left = left of *any* of the widget of interest cells
-  */
+
+  /**
+   * Recomputes and then places each window in its correct position
+   */
+  function re_place_windows() {
+    var windows = $('.gs-w');
+    for (var i = windows.length - 1; i >= 0; i--) {
+      nodes = get_rows_cols({
+        x: parseInt($(windows[i]).attr('data-col')),
+        y: parseInt($(windows[i]).attr('data-row')),
+        sizex: parseInt($(windows[i]).attr('data-sizex')),
+        sizey: parseInt($(windows[i]).attr('data-sizey'))
+      });
+
+    };
+  }
+
+  /**
+   * returns the number unique windows to the left, right, above, and below the given widget
+   * widget -> x, y, id
+   */
+  function get_windows(widget) {
+    var nodesInCol = 0;
+    var nodesInRow = 0;
+    var windows = $('gs-w');
+    for(var j = 0; j < windows.length; j++) { 
+      if (widget.id != $(windows[j]).attr('id')) {
+        if( widget.x == parseInt($(windows[j]).attr('data-row')) 
+            || ( widget.x-1 <= parseInt($(windows[j]).attr('data-row'))-2+parseInt($(windows[j]).attr('data-sizey')) 
+                && widget.y >= parseInt($(windows[j]).attr('data-row')) ) ) 
+        {
+          //console.log($(windows[i]).attr('id') + " is in the same row as " + $(windows[j]).attr('id'));
+          nodesInRow++;
+        }
+        if(widget.y == parseInt($(windows[j]).attr('data-col'))
+            || (widget.y-1 <= parseInt($(windows[j]).attr('data-col'))-2+parseInt($(windows[j]).attr('data-sizex'))
+                && widget.y >= parseInt($(windows[j]).attr('data-col')) ) ) 
+        {
+          //console.log($(windows[i]).attr('id') + " is in the same col as " + $(windows[j]).attr('id'));
+          nodesInCol++;
+        }
+      }
+    }
+    var newHTML = '<div><p>' + widget.id + '</p><p>nodesInRow:' + nodesInRow + '</p><p>nodesInCol:' + nodesInCol + '</p></div>';
+    $('#'+widget.id).html(newHTML);
+    //console.log(widget.id + ' has ' + nodesInRow + ' other nodes in its row and ' + nodesInCol + ' in its col');
+  }
+
+
+  /**
+   * returns the number of widgets to the left, right, above, and below the requested widget
+   * left = left of *any* of the widget of interest cells
+   * widget -> x, y, sizex, sizey
+   */
   function get_rows_cols(widget) {
     var nodes = {
       left: 0,
@@ -158,29 +202,11 @@ $('body').ready(function(){
         nodes.down++;
       }
     }
-    
-    
-    console.log("id:" + widget.id);
-    console.log(" up:" + nodes.up);
-    console.log(" down:" + nodes.down);
-    console.log(" left:" + nodes.left);
-    console.log(" right:" + nodes.right);
+    var newHTML = $('#'+widget.id).html() + '<p>Up:' + nodes.up + '</p><p>Right:' + nodes.right + '</p><p>Down:' + nodes.down + '</p><p>Left:' + nodes.left + '</p>' + resize_handle_html;
+    $('#'+widget.id).html(newHTML);
     return nodes;
   }
+
 });
 
 
-
-/*
-
--> Provenance Capture
--> Staus Messages
--> Science Input
--> Heat Map
--> CDATWeb
--> ESGF Nodes
--> Model Run
--> Node Selector
--> Charting System Status
-
-*/
