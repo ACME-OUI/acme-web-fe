1bfced1fe719d0bd0cb5316b68fca261acd8b1d9
Matthew Harris <mattbenharris@gmail.com>
2015-02-24 09:24:21 -0800

    MBH: basic cleanup of repo and home page

---
+++ cleanup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import shutil
+import os
+
+shutil.rmtree('admin')
+shutil.rmtree('css')
+shutil.rmtree('filetree')
+shutil.rmtree('fonts')
+shutil.rmtree('images')
+shutil.rmtree('js')
+shutil.rmtree('jspanel')
+
+os.remove('local_settings.pyc')
+os.remove('update.py')
+
