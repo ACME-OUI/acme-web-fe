3df40e0280500d9a7b311869d923b5fb92107523
sterlingbaldwin <baldwin32@llnl.gov>
2015-02-25 12:32:12 -0800

    Nodes have row/col occupied info

--- acme_site/static/js/grid.js
+++ acme_site/static/js/grid.js
@@ -71,42 +71,29 @@ $('body').ready(function(){
     var i = 0;
     var windows = $(".gs-w");
     for(; i < windows.length; i++) {
-      var nodesInCol = 1;
-      var nodesInRow = 1;
       var j = 0;
-      get_windows({
+      var rowsCols = get_windows({
         id: $(windows[i]).attr('id'),
         x: parseInt($(windows[i]).attr('data-col')),
         y: parseInt($(windows[i]).attr('data-row')),
       });
-     // resize_widget($(windows[i]), )
-      if ( ($(windows[i]).attr('data-col')-1) % (maxCols/nodesInRow) != 0 && nodesInRow < 4) {
-        //console.log('moving ' + $(windows[i]).attr('id') + ' to ' + (parseInt($(windows[i]).attr('data-col'))+(Math.floor(maxCols/nodesInRow) - (parseInt($(windows[i]).attr('data-col'))-1)%Math.floor(maxCols/nodesInRow))));
-        if (nodesInRow == 1) {
-          var newCol = 1;
-        } else {
-          newCol = parseInt($(windows[i]).attr('data-col'))+(Math.floor(maxCols/nodesInRow) - (parseInt($(windows[i]).attr('data-col'))-1)%Math.floor(maxCols/nodesInRow));
-        }
-        /*
-        gridster.mutate_widget_in_gridmap($(windows[i]), {
-          size_x: parseInt($(windows[i]).attr('data-sizex')),
-          size_y: parseInt($(windows[i]).attr('data-sizey')),
-          col: parseInt($(windows[i]).attr('data-col')),
-          row: parseInt($(windows[i]).attr('data-row'))
-        }, {
-          size_x: parseInt($(windows[i]).attr('data-sizex')),
-          size_y: parseInt($(windows[i]).attr('data-sizey')),
-          col: newCol,
-          row: parseInt($(windows[i]).attr('data-row'))
-        });
-*/
-      }
-      get_rows_cols({
+      var nodes = get_rows_cols({
         id: $(windows[i]).attr('id'),
         x: parseInt($(windows[i]).attr('data-col')),
         y: parseInt($(windows[i]).attr('data-row')),
         sizex: parseInt($(windows[i]).attr('data-sizex')),
-        sizey: parseInt($(windows[i]).attr('data-sizey'))
+        sizey: parseInt($(windows[i]).attr('data-sizey')),
+      });
+      re_place_window({
+        x: $(windows[i]).attr('data-col'),
+        y: $(windows[i]).attr('data-row'),
+        id: $(windows[i]).attr('id'),
+        nodesInRow: rowsCols.row,
+        nodesInCol: rowsCols.col,
+        occupiedLeft: nodes.left,
+        occupiedRight: nodes.right,
+        occupiedUp: nodes.up,
+        occupiedDown: nodes.down,
       });
     }
   }
@@ -115,18 +102,10 @@ $('body').ready(function(){
 
   /**
    * Recomputes and then places each window in its correct position
+   * widget -> x, y, id, sizex, sizey, nodesInRow, nodesInCol, occupiedLeft, occupiedRight, occupiedUp, occupiedDown
    */
-  function re_place_windows() {
-    var windows = $('.gs-w');
-    for (var i = windows.length - 1; i >= 0; i--) {
-      nodes = get_rows_cols({
-        x: parseInt($(windows[i]).attr('data-col')),
-        y: parseInt($(windows[i]).attr('data-row')),
-        sizex: parseInt($(windows[i]).attr('data-sizex')),
-        sizey: parseInt($(windows[i]).attr('data-sizey'))
-      });
-
-    };
+  function re_place_window(widget) {
+    
   }
 
   /**
@@ -134,21 +113,21 @@ $('body').ready(function(){
    * widget -> x, y, id
    */
   function get_windows(widget) {
-    var nodesInCol = 0;
-    var nodesInRow = 0;
-    var windows = $('gs-w');
+    var nodesInCol = 1;
+    var nodesInRow = 1;
+    var windows = $('.gs-w');
     for(var j = 0; j < windows.length; j++) { 
       if (widget.id != $(windows[j]).attr('id')) {
-        if( widget.x == parseInt($(windows[j]).attr('data-row')) 
-            || ( widget.x-1 <= parseInt($(windows[j]).attr('data-row'))-2+parseInt($(windows[j]).attr('data-sizey')) 
-                && widget.y >= parseInt($(windows[j]).attr('data-row')) ) ) 
+        if( widget.y == parseInt($(windows[j]).attr('data-row')) 
+            || ( parseInt($(windows[j]).attr('data-row')) <= widget.y 
+                && widget.y <= ( parseInt($(windows[j]).attr('data-row'))+parseInt($(windows[j]).attr('data-sizey'))-1) ) ) 
         {
           //console.log($(windows[i]).attr('id') + " is in the same row as " + $(windows[j]).attr('id'));
           nodesInRow++;
         }
-        if(widget.y == parseInt($(windows[j]).attr('data-col'))
-            || (widget.y-1 <= parseInt($(windows[j]).attr('data-col'))-2+parseInt($(windows[j]).attr('data-sizex'))
-                && widget.y >= parseInt($(windows[j]).attr('data-col')) ) ) 
+        if(widget.x == parseInt($(windows[j]).attr('data-col'))
+            || ( parseInt($(windows[j]).attr('data-col')) <= widget.x 
+                && widget.x <= (parseInt($(windows[j]).attr('data-col'))+parseInt($(windows[j]).attr('data-sizex'))-1) ) )
         {
           //console.log($(windows[i]).attr('id') + " is in the same col as " + $(windows[j]).attr('id'));
           nodesInCol++;
@@ -157,7 +136,10 @@ $('body').ready(function(){
     }
     var newHTML = '<div><p>' + widget.id + '</p><p>nodesInRow:' + nodesInRow + '</p><p>nodesInCol:' + nodesInCol + '</p></div>';
     $('#'+widget.id).html(newHTML);
-    //console.log(widget.id + ' has ' + nodesInRow + ' other nodes in its row and ' + nodesInCol + ' in its col');
+    return {
+      row: nodesInRow,
+      col: nodesInCol
+    };
   }
 
 
@@ -165,6 +147,9 @@ $('body').ready(function(){
    * returns the number of widgets to the left, right, above, and below the requested widget
    * left = left of *any* of the widget of interest cells
    * widget -> x, y, sizex, sizey
+
+   * TODO: Make sure it scans to the left no only from the origin of the node, but to the left
+          of each grid it extends down, and like wise for right, down, up
    */
   function get_rows_cols(widget) {
     var nodes = {
@@ -174,15 +159,13 @@ $('body').ready(function(){
       down: 0 //below the widget on the page, higher row number
     };
     var windows = $('.gs-w');
-    var i = 0;
-    var highestRow = 0;
-    for( i = 0; i < windows.length; i++) {
+    var highestRow = 1;
+    for( var i = 0; i < windows.length; i++) {
       if(parseInt($(windows[i]).attr('data-row')) + parseInt($(windows[i]).attr('data-sizey')) > highestRow ) {
         highestRow = parseInt($(windows[i]).attr('data-row')) + parseInt($(windows[i]).attr('data-sizey'));
       }
     }
-    var j = 0;
-    for(; j < widget.x; j++) { //scan to the left of the widget.x position
+    for( var j = 1; j < widget.x; j++) { //scan to the left of the widget.x position
       if( gridster.is_widget(j, widget.y)) {
         nodes.left++;
       }
