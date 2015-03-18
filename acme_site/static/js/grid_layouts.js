d7763303a8b369232620a3fd265d4cd9e0fdb4eb
sterlingbaldwin <baldwin32@llnl.gov>
2015-03-18 15:01:44 -0700

    starts the layout

---
+++ acme_site/static/js/grid_layouts.js
@@ -0,0 +1,748 @@
+
+/**
+ * returns the layout specifications for a balanced layout
+ * canvasSizeX, canvasSizeY are the size of the grid canvas
+ * --> Im only doing this for 9 grids right now until I get the names for the next 3
+ */
+function returnBalanced(canvasSizeX, canvasSizeY){
+
+	return [
+		[{ //1 grid
+			row:1,
+			col:1,
+			sizex:canvasSizeX,
+			sizey:canvasSizeY,
+		}],
+		[{ //2 grids
+			row:1,
+			col:1,
+			sizex:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2);
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizey:canvasSizeY
+		},{
+			row:1,
+			col:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2);
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizex:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizey:canvasSizeY
+		}],
+		[{ //3 grids
+			row:1,
+			col:1,
+			sizex:canvasSizeX,
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			col:1,
+			sizex:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2);
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			col:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2);
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizex:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		}],
+		[{ //4 grids
+			row:1, //top left
+			col:1,
+			sizex:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2);
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:1, //top right
+			col:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			},
+			sizex:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:function(canvasSizeX){ //bottom left
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2);
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			col:1
+			sizex:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2);
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:function(canvasSizeX){ // bottom right
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			col:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			},
+			sizex:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		}],
+		[{ //5 grids
+			row:1, //top left
+			col:1,
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2);
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:1, //top right
+			col:function(canvasSizeX){ 
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2);
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizex:function(canvasSizeX){
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:function(canvasSizeY){ //bottom left
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			},
+			col:1,
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(canvasSizeX/3);
+				} else {
+					return canvasSizeX/3;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:function(canvasSizeY){ //bottom middle
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			},
+			col:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(canvasSizeX/3)+1;
+				} else {
+					return canvasSizeX/3;
+				}
+			},
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(canvasSizeX/3)+1;
+				} else {
+					return canvasSizeX/3;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:function(canvasSizeY){ //bottom right
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			},
+			col:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(2*canvasSizeX/3)+1;
+				} else {
+					return 2*anvasSizeX/3;
+				}
+			},
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(canvasSizeX/3)+1;
+				} else {
+					return canvasSizeX/3;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		}],
+		[{ //6 grids
+			row:1, //top left
+			col:1,
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(canvasSizeX/3);
+				} else {
+					return canvasSizeX/3;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:1, // top middle
+			col:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(canvasSizeX/3);
+				} else {
+					return canvasSizeX/3;
+				}
+			},
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(2*canvasSizeX/3)+1;
+				} else {
+					return 2*canvasSizeX/3;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:1, //top right
+			col:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(2*canvasSizeX/3);
+				} else {
+					return 2*canvasSizeX/3;
+				}
+			},
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(2*canvasSizeX/3)+1;
+				} else {
+					return 2*canvasSizeX/3;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:function(canvasSizeX){ //bottom left
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			col:1,
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(2*canvasSizeX/3)+1;
+				} else {
+					return 2*canvasSizeX/3;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:function(canvasSizeX){ //bottom middle
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			col:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(canvasSizeX/3);
+				} else {
+					return canvasSizeX/3;
+				}
+			},
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(2*canvasSizeX/3)+1;
+				} else {
+					return 2*canvasSizeX/3;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:function(canvasSizeX){ //bottom right
+				if(canvasSizeX%2 != 0){
+					return Math.floor(canvasSizeX/2)+1;
+				} else {
+					return canvasSizeX/2;
+				}
+			},
+			col:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(2*canvasSizeX/3);
+				} else {
+					return 2*canvasSizeX/3;
+				}
+			},
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%3 != 0){
+					return Math.floor(2*canvasSizeX/3)+1;
+				} else {
+					return 2*canvasSizeX/3;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		}],
+		[{ //7 grids
+			row:1, //top left
+			col:1,
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%4 != 0){
+					return Math.floor(canvasSizeX/4);
+				} else {
+					return canvasSizeX/4;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:1, // top middle left
+			col:function(canvasSizeX){ 
+				if(canvasSizeX%4 != 0){
+					return Math.floor(canvasSizeX/4);
+				} else {
+					return canvasSizeX/4;
+				}
+			},
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%4 != 0){
+					return Math.floor(canvasSizeX/4);
+				} else {
+					return canvasSizeX/4;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},
+		{
+			row:1, // top middle right
+			col:function(canvasSizeX){ 
+				if(canvasSizeX%4 != 0){
+					return Math.floor(2*canvasSizeX/4);
+				} else {
+					return 2*canvasSizeX/4;
+				}
+			},
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%4 != 0){
+					return Math.floor(canvasSizeX/4);
+				} else {
+					return canvasSizeX/4;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},
+		{
+			row:1, //top right
+			col:function(canvasSizeX){ 
+				if(canvasSizeX%4 != 0){
+					return Math.floor(3*canvasSizeX/4);
+				} else {
+					return 3*canvasSizeX/4;
+				}
+			},
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%4 != 0){
+					return Math.floor(canvasSizeX/4)+canvasSizeX%4;
+				} else {
+					return canvasSizeX/4;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},
+		{
+			row: //bottom left
+			col:
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%4 != 0){
+					return Math.floor(canvasSizeX/4);
+				} else {
+					return canvasSizeX/4;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},
+		{
+			row: //bottom middle
+			col:
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%4 != 0){
+					return Math.floor(canvasSizeX/4);
+				} else {
+					return canvasSizeX/4;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},
+		{
+			row: //bottom right
+			col:
+			sizex:function(canvasSizeX){ 
+				if(canvasSizeX%4 != 0){
+					return Math.floor(canvasSizeX/4);
+				} else {
+					return canvasSizeX/4;
+				}
+			},
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2)+1;
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		}],
+		[{ //8 grids
+			row:
+			col:
+			sizex:
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:
+			col:
+			sizex:
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:
+			col:
+			sizex:
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:
+			col:
+			sizex:
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:
+			col:
+			sizex:
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:
+			col:
+			sizex:
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:
+			col:
+			sizex:
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		},{
+			row:
+			col:
+			sizex:
+			sizey:function(canvasSizeY){
+				if(canvasSizeY%2 != 0){
+					return Math.floor(canvasSizeY/2);
+				} else {
+					return canvasSizeY/2;
+				}
+			}
+		}],
+		[{ //9 grids
+			row:
+			col:
+			sizex:
+			sizey:
+		},{
+			row:
+			col:
+			sizex:
+			sizey:
+		},{
+			row:
+			col:
+			sizex:
+			sizey:
+		},{
+			row:
+			col:
+			sizex:
+			sizey:
+		},{
+			row:
+			col:
+			sizex:
+			sizey:
+		},{
+			row:
+			col:
+			sizex:
+			sizey:
+		},{
+			row:
+			col:
+			sizex:
+			sizey:
+		},{
+			row:
+			col:
+			sizex:
+			sizey:
+		},{
+			row:
+			col:
+			sizex:
+			sizey:
+		}]
+	];
+}
