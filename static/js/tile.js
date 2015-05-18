93480a8cabecc7cfccab361a601f2ce18a0d5462
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-05-18 16:06:12 -0700

    Fixes the color of the spinner for node search

--- static/js/tile.js
+++ static/js/tile.js
@@ -224,9 +224,15 @@ $(document).ready(function(){
     
     });
 
+
+    /* Initializes the connection to the velo api
+     *
+     *
+     *
+     */
     function initVeloConnection(){
     	$.getScript("static/js/spin.js", function(){
-    		if(mode = 'night'){
+    		if(mode == 'night'){
 				var color = '#fff';
 			} else {
 				color = '#000';
@@ -278,9 +284,15 @@ $(document).ready(function(){
     	});
     }
 
+
+    /* populates additional node info by querying the server 
+     * 
+     * id -> the name of the node to fetch information for
+     *
+     */
     function populateNodeSelect(id){
 		$.getScript("static/js/spin.js", function(){
-			if(mode = 'night'){
+			if(mode == 'night'){
 				var color = '#fff';
 			} else {
 				color = '#000';
@@ -309,10 +321,9 @@ $(document).ready(function(){
 	    		node: id
 	    	}
 	    	data = JSON.stringify(data);
-	    	var csrfToken = getCookie('csrftoken');
 			$.ajaxSetup({
 				beforeSend: function(xhr){
-					xhr.setRequestHeader('X-CSRFToken', csrfToken);
+					xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken'));
 				}
 			});
 	    	$.ajax({
@@ -358,13 +369,20 @@ $(document).ready(function(){
 				}
 			});
 		});
-    	
-
     }
 
+    /* Creates the window for searching a node for its data
+     * 
+     * hostname -> the hostname of the node to search
+     *
+     */
     function nodeSearch(hostname){
     	$.getScript("static/js/spin.js", function(){
-    		
+    		if(mode == 'night'){
+				var color = '#fff';
+			} else {
+				color = '#000';
+			}
     		if($('#nodeSearch_window').length != 0){
 	    		$('#nodeSearch_window').find('.tile-contents').empty();
 	    	} else {
@@ -372,11 +390,6 @@ $(document).ready(function(){
 	    		add_tile(new_tile, "nodeSearch_window");
 	    	}
 	    	var searchWindow = $('#nodeSearch_window').find('.tile-contents');
-	    	if(mode = 'night'){
-				var color = '#fff';
-			} else {
-				color = '#000';
-			}
 	    	var opts = {
 				lines: 17, // The number of lines to draw
 				length: 40, // The length of each line
@@ -472,7 +485,7 @@ $(document).ready(function(){
 
     function esgfSearch(searchTerms,hostname){
     	$.getScript("static/js/spin.js", function(){
-    		if(mode = 'night'){
+    		if(mode == 'night'){
 				var color = '#fff';
 			} else {
 				color = '#000';
@@ -1674,6 +1687,7 @@ Left slide menu
 	}
 
 	function setNight(){
+		mode = 'night'
 		$('body').attr({
 			'class':'night'
 		});
@@ -1692,6 +1706,7 @@ Left slide menu
 	}
 
 	function setDay(){
+		mode = 'day'
 		$('body').attr({
 			'class':'day'
 		});
