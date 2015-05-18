d233b813e6db54f5c82db8522af9bccf64f4d987
Sam Fries <samuelbfries@gmail.com>
2015-05-18 09:54:57 -0700

    Basic issue submitting page made; loads questions in sequence

--- acme_issues/urls.py
+++ acme_issues/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
+    url(r'^/?$', views.make_issue),
+    url(r'^question/(\d+)/next$', views.get_next),
     url(r'^questions/create/?$', views.create_question),
     url(r'^questions/delete/(\d+)/?$', views.delete_question),
     url(r'^questions/edit/(\d+)/?$', views.edit_question),
--- acme_issues/views.py
+++ acme_issues/views.py
@@ -4,20 +4,65 @@ from django.template import RequestContext, loader
 from models import *
 import json
 
+# Decorator that passes parsed HTTP body as a kwarg (json_data=) if it's present
+def expects_json(f):
+	def wrapper(request, *args, **kwargs):
+		if request.META.get("CONTENT_TYPE", None) == "application/json":
+			data = request.body
+			data = json.loads(data)
+			kwargs["json_data"] = data
+		return f(request, *args, **kwargs)
+	return wrapper
+
+
 ##### General
 def render_template(request, template, context):
     template = loader.get_template(template)
     context = RequestContext(request, context)
     return template.render(context)
 
+def make_issue(request):
+	questions = CategoryQuestion.objects.all()
+	roots = [q for q in questions if q.is_root()]
+	return HttpResponse(render_template(request, "issues/issue.html", {"root_questions":roots}))
+
+def get_next(request, id):
+	try:
+		q = CategoryQuestion.objects.get(id=id)
+		data = request.GET
+
+		if "yes" not in data:
+			print "defaulting to true"
+			yes = True
+		else:
+			yes = data["yes"] == "true"
+
+		if yes:
+			q = q.get_yes()
+		else:
+			q = q.get_no()
+		
+		data = {"id": q.id}
+		
+		if type(q) == CategoryQuestion:
+			data["question"] = q.question
+		else:
+			data["category"] = q.name
+
+		return HttpResponse(json.dumps(data))
+
+	except CategoryQuestion.DoesNotExist:
+		return HttpResponseBadRequest(json.dumps({"reason":"Question %d does not exist" % id}))
+	except AttributeError:
+		return HttpResponseBadRequest(json.dumps({"reason":"Question %d has no %s value" % (int(id), "yes" if request.GET["yes"] else "no")}))
+
 # Create your views here.
 def show_question(request, source):
 	s = IssueSource.objects.get(name__iexact=source)
+	# Root question of every group of categories has no "no" value.
+	questions = CategoryQuestion.objects.filter(no=None)
 	questions = CategoryQuestion.objects.all()
-	roots = []
-	for q in questions:
-		if q.is_root():
-			roots.append(q)
+	roots = [q for q in questions if q.is_root()]
 	return HttpResponse(render_template(request, "issues/questions.html", {"source":s, "root_questions":roots}))
 
 from templatetags.issues import render_question_tree
