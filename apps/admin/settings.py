c227b3b617820d6b275c283b70a9e3762fe136ba
Sam Fries <samuelbfries@gmail.com>
2015-05-20 11:59:19 -0700

    Moved messages into base.html so everyone can use them

--- apps/admin/settings.py
+++ apps/admin/settings.py
@@ -151,8 +151,11 @@ LOGGING = {
 }
 
 from django.contrib.messages import constants as message_constants
-MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
-                message_constants.INFO: 'info',
-                message_constants.SUCCESS: 'success',
-                message_constants.WARNING: 'warning',
-                message_constants.ERROR: 'danger',}
+
+MESSAGE_TAGS = {
+    message_constants.DEBUG: 'alert-debug',
+    message_constants.INFO: 'alert-info',
+    message_constants.SUCCESS: 'alert-success',
+    message_constants.WARNING: 'alert-warning',
+    message_constants.ERROR: 'alert-danger',
+}
