602cfe31f497eed48009628fa70b09a55e1d0f12
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-05-20 11:08:58 -0700

    updates admin/urls.py to conform to pep8

--- apps/admin/urls.py
+++ apps/admin/urls.py
@@ -3,11 +3,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'admin.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^acme/', include('web_fe.urls')),
-    url(r'^issues/', include('issues.urls'))
+                        url(r'^admin/', include(admin.site.urls)),
+                        url(r'^acme/', include('web_fe.urls')),
+                        url(r'^issues/', include('issues.urls'))
 )
