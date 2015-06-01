fe882746083846ea63819a65a5bdc4e6ce8de4b0
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-06-01 09:35:03 -0700

    Fixes get_file.py to pep8

--- apps/velo/get_file.py
+++ apps/velo/get_file.py
@@ -1,16 +1,17 @@
 import VeloAPI
 import os
 
-def get_file(filename, filepath):
-	velo_api = VeloAPI.Velo()
-	if not velo_api.isJVMStarted():
-	    velo_api.start_jvm()
-	path = os.getcwd() + '/userdata/' + request.user
-	if not os.path.isdir(path):
-	    os.makedirs(path)
 
-	if velo.download_file(filepath, filename):
-	    content = open(path + file_to_get['filename']).read()
-	    print content
-	else:
-		return -1
+def get_file(filename, filepath):
+    velo_api = VeloAPI.Velo()
+    if not velo_api.isJVMStarted():
+        velo_api.start_jvm()
+    path = os.getcwd() + '/userdata/' + request.user
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+    if velo.download_file(filepath, filename):
+        content = open(path + file_to_get['filename']).read()
+        print content
+    else:
+        return -1
