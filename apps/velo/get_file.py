3a393022c4ec664492af464f65b77520016caadb
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-06-01 14:58:28 -0700

    working with get_file

--- apps/velo/get_file.py
+++ apps/velo/get_file.py
@@ -12,9 +12,10 @@ def get_file(remote_file_path, local_path, filename, site_user, velo_username, p
 
         if velo_api.download_file(remote_file_path, local_path):
             content = open(local_path + filename).read()
-            c = content.splitlines(True)
-            for line in c:
-                print line
+            print content
+            # c = content.splitlines(True)
+            # for line in c:
+            #     print line
         else:
             return -1
     except:
