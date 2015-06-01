de3bf0fd92f25ca88cfcf2d1b9dab29438c1f75a
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-06-01 10:26:38 -0700

    working with get_file

--- apps/velo/get_file.py
+++ apps/velo/get_file.py
@@ -3,18 +3,18 @@ import os
 import sys
 
 
-def get_file(filename, filename, user, username, password):
+def get_file(remote_file_path, filename, site_user, velo_username, password):
     velo_api = VeloAPI.Velo()
     if not velo_api.isJVMStarted():
         velo_api.start_jvm()
-    rs = velo_api.init_velo(username, password)
-    path = os.getcwd() + '/userdata/' + user
-    print 'path to file ', path
-    if not os.path.isdir(path):
-        os.makedirs(path)
+    rs = velo_api.init_velo(velo_username, password)
+    local_path = os.getcwd() + '/userdata/' + site_user
+    print 'path to file ', local_path
+    if not os.path.isdir(local_path):
+        os.makedirs(local_path)
 
-    if velo_api.download_file(filename, path):
-        content = open(path + filename).read()
+    if velo_api.download_file(remote_file_path, local_path):
+        content = open(local_path + filename).read()
         print content
     else:
         return -1
