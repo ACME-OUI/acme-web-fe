4bccd984ea399f7a15397de3c7471ff181b0d29e
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-06-01 11:37:28 -0700

    working with get_file

--- apps/velo/get_file.py
+++ apps/velo/get_file.py
@@ -10,17 +10,17 @@ def get_file(remote_file_path, filename, site_user, velo_username, password):
     rs = velo_api.init_velo(velo_username, password)
     local_path = os.getcwd() + '/userdata/' + site_user
 
-    if not os.path.isdir(local_path):
-        os.makedirs(local_path)
-
     path = local_path.split('/')
+    remote_path = remote_file_path.split('/')
+    user_folder_index = remote_path.index(site_user)
     prefix = ''
     for i in range(path.index(site_user)):
         prefix += path[i] + '/'
 
-    for i in range(path.index(site_user), len(path) - 1):
-        if not os.path.isdir(prefix + path[i]):
-            prefix += path[i] + '/'
+    for i in range(user_folder_index, len(remote_path) - 1):
+        if not os.path.isdir(prefix + remote_path[i]):
+            prefix += remote_path[i] + '/'
+            print 'making new dir ', prefix
             os.makedirs(prefix)
 
     if velo_api.download_file(remote_file_path, local_path):
