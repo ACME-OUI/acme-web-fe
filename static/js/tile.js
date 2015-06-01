0f621b1c3bf33ec3a7fdcbb2ca26a8bf9ae7f741
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-06-01 13:20:57 -0700

    working with get_file

--- static/js/tile.js
+++ static/js/tile.js
@@ -255,26 +255,54 @@ $(document).ready(function() {
 
 
 	function getFile(id){
-		id = id.replace(/_/g, '/');
-		id = id.replace(/-/g, ' ');
-		id = id.replace(/X/g, '_');
-		id = id.replace(/Z/g, '-');
-		id = id.substring(0, id.length -1);
-		var path = id.split('/');
-		id = '';
-		for(var i = 2; i < path.length; i++){
-			id += path[i] + '/';
-		}
-		id = id.substring(0, id.length -1);
-		console.log('attempting to get file:'+id);
-		data = {
-			'file':id
-		}
-		get_data('get_file/', 'POST', data, function(response){
-			console.log(response.responseText);
-		}, function(response){
-			alert('Faild to retrieve file from server');
-		} );
+		$.getScript("static/js/spin.js", function() {
+			if (mode == 'night') {
+				var color = '#fff';
+			} else {
+				color = '#000';
+			}
+			var opts = {
+				lines: 17, // The number of lines to draw
+				length: 40, // The length of each line
+				width: 10, // The line thickness
+				radius: 30, // The radius of the inner circle
+				corners: 1, // Corner roundness (0..1)
+				rotate: 0, // The rotation offset
+				direction: 1, // 1: clockwise, -1: counterclockwise
+				color: color, // #rgb or #rrggbb or array of colors
+				speed: 1, // Rounds per second
+				trail: 100, // Afterglow percentage
+				shadow: false, // Whether to render a shadow
+				hwaccel: false, // Whether to use hardware acceleration
+				className: 'spinner', // The CSS class to assign to the spinner
+				zIndex: 2e9, // The z-index (defaults to 2000000000)
+				top: '50%', // Top position relative to parent
+				left: '50%' // Left position relative to parent
+			};
+			var spinner = new Spinner(opts).spin();
+			document.getElementById('velo-text-edit').appendChild(spinner.el);
+			id = id.replace(/_/g, '/');
+			id = id.replace(/-/g, ' ');
+			id = id.replace(/X/g, '_');
+			id = id.replace(/Z/g, '-');
+			id = id.substring(0, id.length -1);
+			var path = id.split('/');
+			id = '';
+			for(var i = 2; i < path.length; i++){
+				id += path[i] + '/';
+			}
+			id = id.substring(0, id.length -1);
+			console.log('attempting to get file:'+id);
+			data = {
+				'file':id
+			}
+			get_data('get_file/', 'POST', data, function(response){
+				console.log(response.responseText);
+				initCodeMirror();
+			}, function(response){
+				alert('Faild to retrieve file from server');
+			} );
+		});
 	}
 
 	function initFileTree() {
