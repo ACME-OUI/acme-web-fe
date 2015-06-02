6b31d4ac180cf1701784a420dc997047f6ea8d05
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-06-02 11:17:10 -0700

    working on getting codemirror changes to save to velo

--- apps/velo/VeloAPI.py
+++ apps/velo/VeloAPI.py
@@ -67,6 +67,14 @@ class Velo:
             resMgr.bulkUpload(fileServerMap, None)
             return 0
         except:
+            import traceback
+            print '1', e.__doc__
+            print '2', sys.exc_info()
+            print '3', sys.exc_info()[0]
+            print '4', sys.exc_info()[1]
+            print '5', traceback.tb_lineno(sys.exc_info()[2])
+            ex_type, ex, tb = sys.exc_info()
+            print '6', traceback.print_tb(tb)
             return -1
 
     # download file from velo
