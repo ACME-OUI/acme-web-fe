5fa4b6dc850dd0dfad63ff83f118ec83144a27f0
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-06-03 14:20:45 -0700

    Gets the first couple tests running and passing

--- apps/tests/run_tests.py
+++ apps/tests/run_tests.py
@@ -8,21 +8,11 @@ import os
 import sys
 from subprocess import Popen, PIPE
 
-try:
-    import SeleniumLibrary
-except ImportError:
-    print 'SeleniumLibrary module missing'
-    sys.exit(1)
 
-server = 'http://localhost:8000'
+server = 'localhost:8000'
 
 if len(sys.argv) > 1:
     server = sys.argv[1]
-
-
-def start_selenium():
-    log = open('semenium_log.txt', 'w')
-    SeleniumLibrary.start_selenium_server(log)
 
 
 def run_tests(test):
@@ -30,7 +20,10 @@ def run_tests(test):
         ['pybot', '--variable', 'SERVER:' + server, test], stdout=PIPE)
     (out, err) = process.communicate()
     exit_code = process.wait()
+    print out
+
 
 if __name__ == '__main__':
     for test in next(os.walk(os.getcwd()))[1]:
+        print 'Running test', test
         run_tests(test)
