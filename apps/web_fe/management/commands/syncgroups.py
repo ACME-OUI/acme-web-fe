8bd13a0a958378b03a95e2e5b4e01ecb98c169ed
Samuel Fries <fries2@llnl.gov>
2015-06-10 11:30:38 -0700

    Added a setting for some default groups and a command that will automatically sync them

---
+++ apps/web_fe/management/commands/syncgroups.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User, Group, Permission
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = "Sync groups out of settings.py"
+
+    def handle(self, *args, **options):
+        groups = settings.DEFAULT_GROUPS
+        for groupname in groups:
+
+            try:
+                g = Group.objects.get(name=groupname)
+            except Group.DoesNotExist:
+                g = Group()
+                g.name = groupname
+                g.save()
+
+            if g.name == "Default":
+                g.user_set = User.objects.all()
+
+            p_set = []
+            for permission in groups[groupname]:
+                try:
+                    p = Permission.objects.get(codename=permission)
+                    p_set.append(p)
+                except Permission.DoesNotExist:
+                    if permission[0] == "*":
+                        permissions = Permission.objects.filter(content_type__model=permission.split("_")[1])
+                        p_set.extend(permissions)
+            g.permission_set = p_set
+            g.save()
