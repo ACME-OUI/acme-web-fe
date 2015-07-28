618ea5e10adbc7ce625a0394dd0f32a2bbe024e9
Sterling Baldwin <sbaldwin4@mail.csuchico.edu>
2015-07-28 15:25:31 -0700

    Updates the velo_service

--- apps/velo/velo_service.py
+++ apps/velo/velo_service.py
@@ -19,19 +19,22 @@ class VeloService(object):
     def dispatch_request(self, request):
         data = json.loads(request.get_data())
         if data['command'] != 'init':
-            v_str = self.redis.get(data['velo_user'])
-            v = pickle.loads(v_str)
-            return Response(self.commands[data['command']](data, v))
+            if 'velo_user' not in data:
+                return Response('Failed')
+            if data['velo_user'] not in self.velo_instances:
+                return Response('Failed')
+            return Response(self.commands[data['command']](data, self.velo_instances[data['velo_user']]))
         else:
             return Response(self.commands[data['command']](data))
-
+ 
     def init_velo(self, data):
+        if data['velo_user'] in self.velo_instances:
+            return 'Success'
         v = VeloAPI.Velo()
-        v.start_jvm()
-        reply_string = v.init_velo(data['velo_user'], data['velo_pass'])
-        v_str = pickle.dumps(v)
-        self.redis.set(data['velo_user'], v_str)
-        return reply_string
+        self.velo_instances[data['velo_user']] = v
+        VeloAPI.start_jvm()
+        v.init_velo(data['velo_user'], data['velo_pass'])
+        return 'Success'
 
     def get_folder(self, data, velo):
         return velo.get_resources(data['folder'])
@@ -51,6 +54,12 @@ class VeloService(object):
     def delete(self, data, velo):
         return velo.delete_resource(data['resource'])
 
+    def is_initialized(self, data, velo):
+        if data['velo_user'] in self.velo_instances:
+            return 'true'
+        else:
+            return 'false'
+
     def wsgi_app(self, environ, start_response):
         request = Request(environ)
         response = self.dispatch_request(request)
@@ -59,23 +68,21 @@ class VeloService(object):
     def __call__(self, environ, start_response):
         return self.wsgi_app(environ, start_response)
 
-    def __init__(self, config):
-        self.redis = redis.Redis(config['redis_host'], config['redis_port'])
+    def __init__(self):
         self.commands = {
             'init': self.init_velo,
             'get_folder': self.get_folder,
             'create_folder': self.create_folder,
             'save_file': self.save_file,
             'get_file': self.get_file,
-            'delete': self.delete
+            'delete': self.delete,
+            'is_initialized': self.is_initialized
         }
+        self.velo_instances = {}
 
 
-def create_app(redis_host='127.0.0.1', redis_port=6379, with_static=True):
-    app = VeloService({
-        'redis_host':       redis_host,
-        'redis_port':       redis_port
-    })
+def create_app(with_static=True):
+    app = VeloService()
     if with_static:
         app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
             '/static':  os.path.join(os.path.dirname(__file__), 'static')
