69e227da867ca23eea41d621d4873c225126a79f
James-Crean <creanjames@gmail.com>
2016-03-10 12:03:50 -0800

    Adds post feature to poller for changing status

--- apps/poller/tests.py
+++ apps/poller/tests.py
@@ -1,36 +1,52 @@
 import unittest
 import requests
 import os
-
+import json
 
 class Testresponses(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.url = 'http://127.0.0.1:8000/poller/'
 
     def test_get_all(self):
         payload = {'status': 'all'}
-        r = requests.get('http://127.0.0.1:8000/poller/', params=payload)
+        r = requests.get(self.url , params=payload)
         self.assertEqual(str(r), '<Response [200]>')
 
     def test_get_new(self):
         payload = {'status': 'new'}
-        r = requests.get('http://127.0.0.1:8000/poller/', params=payload)
+        r = requests.get(self.url, params=payload)
         self.assertEqual(str(r), '<Response [200]>')
 
     def test_get_in_progress(self):
         payload = {'status': 'in_progress'}
-        r = requests.get('http://127.0.0.1:8000/poller/', params=payload)
+        r = requests.get(self.url, params=payload)
         self.assertEqual(str(r), '<Response [200]>')
 
     def test_get_complete(self):
         payload = {'status': 'complete'}
-        r = requests.get('http://127.0.0.1:8000/poller/', params=payload)
+        r = requests.get(self.url, params=payload)
         self.assertEqual(str(r), '<Response [200]>')
 
     def test_get_failed(self):
         payload = {'status': 'failed'}
-        r = requests.get('http://127.0.0.1:8000/poller/', params=payload)
+        r = requests.get(self.url, params=payload)
         self.assertEqual(str(r), '<Response [200]>')
 
+    def test_get_csrf(self):
+        r = requests.get(self.url)
+        self.assertEqual(str(r), '<Response [200]>')
+        csrf = r.cookies['csrftoken']
+    def test_update_status(self):
+        payload = json.dumps({'id': 1, 'status': 'In_progress'})
+        s = requests.Session()
+        r1 = s.get(self.url)
+        self.assertEqual(str(r1), '<Response [200]>')
+        csrf_token = r1.cookies['csrftoken']
+        headers = {'Content-type': 'application/json',  "X-CSRFToken":csrf_token, 'Referer': self.url}
+        r2 = s.post(self.url, data=payload, headers=headers)
+        self.assertEqual(str(r2), '<Response [200]>')
 
 os.system('clear')
 suite = unittest.TestLoader().loadTestsFromTestCase(Testresponses)
-unittest.TextTestRunner(verbosity=2).run(suite)
+unittest.TextTestRunner(verbosity=1).run(suite)
--- apps/poller/views.py
+++ apps/poller/views.py
@@ -1,8 +1,12 @@
+from django.shortcuts import render
+from django.core.context_processors import csrf
+from django.views.decorators.csrf import csrf_exempt
+from django.template import Template
 from django.http import HttpResponse
 from django.http import JsonResponse
 from poller.models import UserRuns
 import json
-
+import pdb
 
 def index(request):
     if request.method == 'GET':
@@ -21,7 +25,7 @@ def index(request):
                 elif status == 'failed':
                     data = UserRuns.objects.filter(status='failed')
                 else:
-                    print "got here"
+                    print "Invalid status recieved"
                     return HttpResponse(status=404)
                 obj_list = []
                 for obj in data:
@@ -29,16 +33,30 @@ def index(request):
                     obj_dict['runspec'] = obj.runspec
                     obj_list.append(obj_dict)
                 return JsonResponse(obj_list, safe=False)
+            else:
+                return HttpResponse(render(request, "poller/token.html", {}))
         except Exception as e:
-            print_debug(e)
+            print e
             return HttpResponse(status=500)
 
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            new_run = UserRuns.objects.create(user=data['user'], runspec=data['runspec'], status='new')
-            new_run.save
-            return HttpResponse("Successfully posted ")
+            if 'id' in data:
+                db_id = data['id']
+                newstatus = data['status']
+                try:
+                    entry = UserRuns.objects.get(id=db_id)
+                    entry.status = newstatus
+                    entry.save()
+                except Exception as e:
+                    print e
+                    return HttpResponse(status=500)
+                return HttpResponse("Success")
+            else:
+                new_run = UserRuns.objects.create(user=data['user'], runspec=data['runspec'], status='new')
+                new_run.save()
+                return HttpResponse("Successfully updated status")
         except Exception as e:
-            print_debug(e)
+            print e
             return HttpResponse(status=500)
