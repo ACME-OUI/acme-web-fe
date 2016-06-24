209c18ac5d9152c458a5c6acba8949189c098c5d
Sterling Baldwin <sterling16@mac.com>
2016-06-24 16:06:00 -0700

    Starts the frontend refactor, adds Golden-Layout

---
+++ static/js/controllers/cdat.js
@@ -0,0 +1,16 @@
+
+angular.module('cdat', [])
+.controller('CDATControl', ['$scope', '$http', function($scope, $http) {
+
+
+  $scope.new_plot = function new_plot(id, plotvars) {
+		console.log("making cdat window");
+
+		var elem = $('#' + id + " .plot-content");
+		elem.append(cdat.make_plot_panel());
+		if (plotvars && typeof plotvars !== "undefined") {
+			elem.find(".cdat-graphic-method").text(plotvars.method);
+			elem.find(".cdat-graphic-template").text(plotvars.template);
+		}
+	}
+}])
