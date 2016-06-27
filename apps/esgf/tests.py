9db3cbf4691256ba04da6151bb4d2954fe65da34
Sterling Baldwin <sterling16@mac.com>
2016-06-27 14:25:06 -0700

    Working on getting the esgf backend working again so I can connect to the front window system

--- apps/esgf/tests.py
+++ apps/esgf/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+import unittest
+import json
+import requests
 
-# Create your tests here.
+
+class ESGFTestCase(LiveServerTestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.username = 'https://pcmdi.llnl.gov/esgf-idp/openid/acmetest'
+        self.password = 'ACM#t3st'
+
+    def test_logon(self):
+        credential = json.dumps({
+            'username': self.username,
+            'password': self.password
+        })
+        response_code = requests.get(self.live_server_url, data=credential).status_code
+        self.assertTrue( response_code == requests.codes.ok)
