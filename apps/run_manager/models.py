b1c04088634b0e833ecda6e7f6461ad5afe92618
Sterling Baldwin <sterling16@mac.com>
2016-07-11 11:12:31 -0700

    Adds update_script to run_manager view, not tested yet

--- apps/run_manager/models.py
+++ apps/run_manager/models.py
@@ -5,3 +5,15 @@ class ModelRun(models.Model):
     user = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
     edited = models.TextField()
+
+class RunScript(models.Model):
+    
+    class Meta:
+        get_latest_by = 'version'
+
+    user = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.TextField()
+    name = models.CharField(max_length=100)
+    version = models.IntegerField()
+    run = models.CharField(max_length=100)
