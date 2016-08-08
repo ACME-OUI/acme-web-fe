4abdd32295d860dd4fdc01e2eac7e8f9fc3a3617
sterlingbaldwin <sterling16@mac.com>
2016-08-08 13:52:44 -0700

    Removes http requests to poller in favor of calling the view directly

--- apps/run_manager/constants.py
+++ apps/run_manager/constants.py
@@ -14,5 +14,7 @@ RUN_CONFIG_DEFAULT_PATH = '/resources/run_config_default/'
 # hostname and port for the frontend poller
 POLLER_URL = 'http://localhost:8000/poller/update/'
 
+POLLER_HOST = 'http://localhost'
+POLLER_SUFFIX = '/poller/update/'
 # location to look for run output
 DIAG_OUTPUT_PREFIX = '/Users/baldwin32/projects/acme-web-fe/userdata/'
