d681455801518c91d594553db6c591c43e2be1dd
sterlingbaldwin <sterling16@mac.com>
2016-09-23 11:00:29 -0700

    Removes the esgf node model until the node manager can be used

--- apps/esgf/views.py
+++ apps/esgf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from models import ESGFNode
+# from models import ESGFNode
 from models import PublishConfig
 from models import FavoritePlot
 
